ec4e4e9505b90f8e6903476c17d0d6ef97f8cf59
Diogo-Ribeiro-FE <diogocostaofc3@gmail.com>
2026-04-16 09:34:02 +0100

    Implementação do teamdetails.ts e html com a sua respectiva rota

--- mundial-futebol/src/app/app.routes.ts
+++ mundial-futebol/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { TeamsListComponent } from './pages/teams/teams-list/teams-list';
 import { TeamForm } from './pages/teams/team-form/team-form';
+import { TeamDetail } from './pages/teams/team-detail/team-detail';
 
 //para testarmos algo, por exemplo quis testar o teams-list.html, tens que adicionar aqui ás rotas
 
@@ -8,5 +9,6 @@ export const routes: Routes = [
   { path: 'teams', component: TeamsListComponent },
   { path: 'teams/new', component: TeamForm },
   { path: 'teams/edit/:id', component: TeamForm },
+  { path: 'teams/:id', component: TeamDetail },
   { path: '', redirectTo: 'teams', pathMatch: 'full' }
 ];
--- mundial-futebol/src/app/pages/teams/team-detail/team-detail.ts
+++ mundial-futebol/src/app/pages/teams/team-detail/team-detail.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { TeamService } from '../../../core/services/teams';
+import { Team } from '../../../core/models/teams';
 
 @Component({
   selector: 'app-team-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './team-detail.html',
-  styleUrl: './team-detail.css',
+  styleUrl: './team-detail.css'
 })
-export class TeamDetail {
+export class TeamDetail implements OnInit {
+  team: Team | null = null;
+  id: string | null = null;
 
+  constructor(
+    private teamService: TeamService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.teamService.getTeamById(this.id).subscribe(data => {
+        this.team = { ...data };
+        this.cdr.detectChanges();
+      });
+    }
+  }
+
+  deleteTeam() {
+    if (this.id) {
+      this.teamService.deleteTeam(this.id).subscribe(() => {
+        this.router.navigate(['/teams']);
+      });
+    }
+  }
 }
